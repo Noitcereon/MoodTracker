8e24dab72c654b58b9517e1a54717fe2e6212e92
Thomas (Noit) <tba@live.dk>
2021-01-30 18:01:39 +0100

    Published app to test if it worked.

--- MoodTrackerLib/Implementation/Concurrency.cs
+++ MoodTrackerLib/Implementation/Concurrency.cs
@@ -23,8 +23,10 @@ namespace MoodTrackerLib.Implementation
             
             CheckIfDirExists();
 
-            // TODO: it might be better to use FileStream, but can't be bothered figuring out how it works.
-            using StreamReader sr = new StreamReader(FilePath);
+            // TODO: it might be better to use StreamReader, but can't be bothered looking into how it should be done atm.
+            //using StreamReader sr = new StreamReader(FilePath);
+            //sr.ReadLine();
+
             byte[] fileBytes = File.ReadAllBytes(FilePath);
             string json = Encoding.UTF8.GetString(fileBytes);
 
