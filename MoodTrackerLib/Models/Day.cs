431ea0062a06a84f41384ad9f9b2fcc8d731ee7f
Thomas (Noit) <tba@live.dk>
2021-05-27 12:53:26 +0200

    Added a dataannotation to Points.

--- MoodTrackerLib/Models/Day.cs
+++ MoodTrackerLib/Models/Day.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,7 @@ namespace MoodTrackerLib.Models
 
         public DateTimeOffset Date { get; set; }
 
+        [Range(0.0, 10.0)]
         public double Points
         {
             get => _points;
