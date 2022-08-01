1908b7e6b019142065b2ead13200e86293c36bd5
Thomas 'Noit' A <tba@live.dk>
2022-08-01 13:01:42 +0200

    Refactor(nlog): Replace config file with programmatic configuration

--- MoodTrackerConsole/Program.cs
+++ MoodTrackerConsole/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NLog;
+using NLog.Targets;
+using System;
 
 namespace MoodTrackerConsole
 {
@@ -6,7 +8,23 @@ namespace MoodTrackerConsole
     {
         static void Main()
         {
-            NLog.ILogger logger = NLog.LogManager.GetCurrentClassLogger();
+            var config = new NLog.Config.LoggingConfiguration();
+
+            // Targets where to log to: File and Console
+            var fullLogFile = new FileTarget("fullFileLog") { FileName = "fullLog.log" };
+            var errorLog = new FileTarget("errorFileLog") { FileName = "error.log" };
+            var logconsole = new ConsoleTarget("logconsole");
+
+            // Rules for mapping loggers to targets
+            config.AddRule(LogLevel.Error, LogLevel.Fatal, logconsole);
+            config.AddRule(LogLevel.Trace, LogLevel.Fatal, fullLogFile);
+            config.AddRule(LogLevel.Warn, LogLevel.Fatal, errorLog);
+
+            // Apply config           
+            LogManager.Configuration = config;
+
+            ILogger logger = LogManager.GetCurrentClassLogger();
+
             try
             {
                 logger.Debug("NLog Initialized");
